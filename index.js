3a4925b2db17b853bbe8d65c5e3c7733ee0801e3
karthik_005 <karthikeyabh554@gmail.com>
2024-05-16 13:40:15 +0530

    commited successfully

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+
+
 const userRoutes = require("./routes/userRoutes")
 
 const app = express();
