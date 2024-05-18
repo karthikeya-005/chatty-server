7d368333f4a213dee6e4982ecb1f1f85135898bb
karthik_005 <karthikeyabh554@gmail.com>
2024-05-18 15:32:56 +0530

    minor testing changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
-const { register, login, setAvatar, getAllusers } = require('../controlers/userController')
+const { register, login, setAvatar, getAllUsers } = require('../controlers/userController')
 
 const router = require('express').Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/setAvatar/:id', setAvatar)
-router.get('/allusers/:id', getAllusers)
+router.get('/allusers/:id', getAllUsers)
 
 module.exports = router
