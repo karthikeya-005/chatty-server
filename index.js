381348ae6df42428707b01915c1aac5dc1428daf
karthikeya-005 <karthikeyabh554@gmail.com>
2024-06-02 17:05:34 +0530

    creating the chat messaging features server side

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 
 const userRoutes = require("./routes/userRoutes")
+const messageRoute = require("./routes/messagesRoute")
 
 const app = express();
 require("dotenv").config();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes)
+app.use("/api/messages", messageRoute)
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
