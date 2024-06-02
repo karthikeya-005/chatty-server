8520d3a6dff1218df4703e3cd6cf88b633ffa16f
karthikeya-005 <karthikeyabh554@gmail.com>
2024-06-02 17:52:44 +0530

    minor adjustments

--- controlers/messagesController.js
+++ controlers/messagesController.js
@@ -4,13 +4,34 @@ module.exports.addMessage = async (req, res, next) => {
     try {
         const { from, to, message } = req.body
         const data = await messageModel.create({
-            message: { text: message }, users: [from, to],
+            message: { text: message },
+            users: [from, to],
             sender: from,
-        });
-        if (data) return res.json({ msg: "Message added succesfully" });
-        return res.json({msg:"Failed to add the message to the database"})
+        })
+        if (data) return res.json({ msg: 'Message added succesfully' })
+        return res.json({ msg: 'Failed to add the message to the database' })
     } catch (ex) {
         next(ex)
     }
 }
-module.exports.getAllMessage = async (req, res, next) => {}
+module.exports.getAllMessage = async (req, res, next) => {
+    try {
+        const { from, to } = req.body
+        const messages = await messageModel
+            .find({
+                users: {
+                    $all: [from, to],
+                },
+            })
+            .sort({ updatedAt: 1 })
+        const projectedMessages = messages.map((msg) => {
+            return {
+                fromSelf: msg.sender.toString() === from,
+                message: msg.message.text,
+            }
+        })
+        res.json(projectedMessages)
+    } catch (ex) {
+        next(ex)
+    }
+}
--- routes/messagesRoute.js
+++ routes/messagesRoute.js
@@ -5,7 +5,7 @@ const {
 
 const router = require('express').Router()
 
-router.post('/addmsg/', addMessage)
-router.post('/getmsg/', getAllMessage)
+router.post('/addmsg', addMessage)
+router.post('/getmsg', getAllMessage)
 
 module.exports = router
